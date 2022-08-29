fb19afa93fcab4c3021ea667761c001002b49407
Ryan Jesus Valentim de Oliveira <ryan.jesus@hotmail.com>
2022-08-29 16:10:55 -0300

    TESTE

--- main.py
+++ main.py
@@ -24,3 +24,4 @@ print(impostos)
 
 # link tutorial lambda
 #http://www.dsc.ufcg.edu.br/~pet/jornal/maio2013/materias/tutoriais.html#:~:text=Em%20Python%2C%20existe%20um%20conceito,se%20comportar%C3%A1%20como%20uma%20fun%C3%A7%C3%A3o.
+#https://www.youtube.com/watch?v=xmMXULd0dxc
